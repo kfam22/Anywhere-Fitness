a4062ee1809a94075c1dbe1b16acef509972629b
KFamurewa <kayla.famurewa@gmail.com>
2022-03-04 11:07:54 -0500

    reverting to commit: e4fdf8e bc i broke something

---
+++ api/auth/auth-middleware.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../secrets')
+
+  const restricted = (req, res, next) => {
+      const token = req.headers.authorization
+      if(!token) {
+          next({
+              status: 401,
+              message: 'token required!'
+          })
+      } else {
+          jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+              if (err) {
+                  next({
+                      status: 401,
+                      message: 'invalid token'
+                  })
+              } else {
+                //   console.log(decodedToken)
+                  req.decodedToken = decodedToken
+                  next()
+              }
+          })
+      }
+  }
+
+  const only = role => (req, res, next) => {
+      if(req.decodedToken.role === role){
+          next()
+      } else {
+          next({
+              status: 401,
+              message: 'you do not have permissions'
+          })
+      }
+  }
+
+  module.exports = {
+      restricted,
+      only
+  }
